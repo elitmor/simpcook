b3092995eef9dfd4e007818cbf3aab7111e82236
Elitmor <elitmor@gmail.com>
2022-06-13 16:19:40 +0300

    Added calculator

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import '%modules%/header/header';
 import '%modules%/preview-menu/preview-menu';
 import '%modules%/promotion/promotion';
+import '%modules%/calculator/calculator';
 import '%modules%/footer/footer';
